f1f040b4f8766fe7a3616cb244974a64b8081c91
mibriggs <33757644+mibriggs@users.noreply.github.com>
2024-12-07 16:11:12 -0500

    new map

--- src/lib/components/mapPanel.svelte
+++ src/lib/components/mapPanel.svelte
@@ -4,46 +4,24 @@
 	import 'maplibre-gl/dist/maplibre-gl.css';
 	import { addMarkersToMap } from '$lib';
 	import type { Feature, Point } from "geojson";
+	import { PUBLIC_MAP_TILER_KEY } from '$env/static/public';
 
 
 	let { birds }: { birds: EBirdEntry[] } = $props();
 	let mapContainer: HTMLDivElement;
 	let map: Map;
-	// let lastClickedCoordinates: number[] = [];
 
 	$effect(() => {
 		map = new Map({
 			container: mapContainer,
-			style: 'https://demotiles.maplibre.org/style.json',
-			// {
-			//     version: 8,
-			//     sources: {
-			//         MIERUNEMAP: {
-			//             type: 'raster',
-			//             tiles: ['https://tile.mierune.co.jp/mierune_mono/{z}/{x}/{y}.png'],
-			//             tileSize: 256,
-			//             attribution:
-			//                 "Maptiles by <a href='http://mierune.co.jp/' target='_blank'>MIERUNE</a>, under CC BY. Data by <a href='http://osm.org/copyright' target='_blank'>OpenStreetMap</a> contributors, under ODbL."
-			//         }
-			//     },
-			//     layers: [
-			//         {
-			//             id: 'MIERUNEMAP',
-			//             type: 'raster',
-			//             source: 'MIERUNEMAP',
-			//             minzoom: 0,
-			//             maxzoom: 18
-			//         }
-			//     ]
-			// },
-			center: [0, 0],
-			zoom: 2
+			style: `https://api.maptiler.com/maps/streets-v2/style.json?key=${PUBLIC_MAP_TILER_KEY}`,
+			center: [-95, 40],
+			zoom: 4
 		});
 
 		map.addControl(new NavigationControl());
 		map.on("load", () => addMarkersToMap(birds, map));
 		map.on("click", "marker-layer", (event) => {
-			// console.log(`Last clicked coords: ${lastClickedCoordinates}`);
 			let birdsInArea = event.features? [...event.features] : [];
 			if (birdsInArea.length > 0) {
 				const selectedBird: Feature<Point> = birdsInArea[0] as Feature<Point>;
@@ -51,8 +29,6 @@
 				new Popup().setLngLat([coordinates[0], coordinates[1]])
 				.setHTML(`<h3>${selectedBird.properties?.title}</h3><p>${selectedBird.properties?.date}</p>`)
 				.addTo(map);
-				// console.log(`Selected coords: ${coordinates}`);
-				// lastClickedCoordinates = coordinates;
 			}
 		});
 
